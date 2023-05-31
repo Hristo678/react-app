e842891707f4a7482e6dd652362dddd1e0799e74
Hristo <fenimo@abv.bg>
2023-05-31 17:08:16 +0300

    Commiting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+
+import React, {useState, useEffect, useContext} from 'react';
 import './App.css';
 
-function App() {
+import UserContext from './Context';
+
+const App = (props) => {
+
+const [loggedIn, setLoggedIn] = useState(null)
+const [user, setUser] = useState(null)
+const context = useContext(UserContext)
+ 
+
+  const logIn = (user)=>{
+    setLoggedIn(true)
+    setUser(user)
+  }
+
+  const logOut = ()=>{
+    document.cookie = "x-auth-token= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
+    setLoggedIn(false)
+    setUser(null)
+    
+  }
+
+  useEffect(() =>{
+    if(document.cookie){
+      const email = sessionStorage.getItem('email')
+      const password = sessionStorage.getItem('password')
+      logIn({email, password})
+    }
+  },[loggedIn])
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <UserContext.Provider value={{
+      loggedIn,
+      user,
+      logIn,
+      logOut
+    }}>
+      
+        {props.children}
+    </UserContext.Provider>
+  )
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Navigation from './navigation'
+import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App>
+    <BrowserRouter>
+      <Navigation />
+    </BrowserRouter>
+    </App>
   </React.StrictMode>,
   document.getElementById('root')
 );
